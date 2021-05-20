6011c504e690c13f1c5a7fb73178de42b3207bb0
Nicolas García <nicolas.garcia@compucad.com.mx>
2021-05-20 14:10:16 -0500

    Updated frame title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return <div style={{ display: "flex", alignItems:"middle", justifyContent:"center", width:"100%", paddingTop:"1%"}}>
-    <iframe src='https://webchat.botframework.com/embed/mycloudtestbot?s=O0OZkG5Hwe4.d5egNXCbJs2JwQFsNdQJyBF3u4-CNNzxfSSLjnVybAc'
+    <iframe title="My first Azure Bot" src='https://webchat.botframework.com/embed/mycloudtestbot?s=O0OZkG5Hwe4.d5egNXCbJs2JwQFsNdQJyBF3u4-CNNzxfSSLjnVybAc'
       style={{ width: "30%", display: "block", height: "80vh" }}>
 
     </iframe>
