65a180dde0e6ddc2556ec4a54e1dcf05ae3462fe
Nicolas García <nicolas.garcia@compucad.com.mx>
2021-05-20 13:57:42 -0500

    Added bot frame

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div style={{ display: "flex", alignItems:"middle", justifyContent:"center", width:"100%", paddingTop:"1%"}}>
+    <iframe src='https://webchat.botframework.com/embed/mycloudtestbot?s=O0OZkG5Hwe4.d5egNXCbJs2JwQFsNdQJyBF3u4-CNNzxfSSLjnVybAc'
+      style={{ width: "30%", display: "block", height: "80vh" }}>
+
+    </iframe>
+  </div>;
 }
 
 export default App;
